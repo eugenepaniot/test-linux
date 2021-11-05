2506ead27fd800d95da3f0d0e233b0015f4947e4
rtoax <2386499836@qq.com>
2021-11-05 10:47:45 +0800

    request_irq 共享中断线

--- interrupt/request_irq/request-irq.c
+++ interrupt/request_irq/request-irq.c
@@ -1,5 +1,5 @@
 /**
- *	File ./request-irq.c 
+ *	File request-irq.c 
  *	Time 2021.11.03
  *	Author Rong Tao <rongtao@cestc.cn>
  */
@@ -14,32 +14,42 @@ MODULE_AUTHOR("Rong Tao");
 MODULE_DESCRIPTION("A Simple request irq module");
 MODULE_VERSION("0.1");
 
+static int irqnum = 2;
+module_param(irqnum, int, 0644);
+MODULE_PARM_DESC(irqnum, "irq number, acpi is OK");
+
 static char *name = "testirq";
 module_param( name, charp, S_IRUGO);
 MODULE_PARM_DESC(name, "irq name");	
 
-/**
- *  找一个 /proc/interrupt 中没有的 中断号
- */
-#define IRQ_NUM 2
+struct myirq {
+	int devid;
+};
+struct myirq mydev = {1119};
 
 irqreturn_t no_action(int cpl, void *dev_id)
 {
-    printk(KERN_INFO "cpl %d!\n", cpl);
-	printk(KERN_INFO "in_irq = %ld\n", in_irq());
-	printk(KERN_INFO "in_hardirq = %ld\n", in_hardirq());
-	return IRQ_NONE;
+    printk(KERN_INFO "irq line number is %d!\n", cpl);
+	printk(KERN_INFO "in_task       = %d\n", in_task());
+	printk(KERN_INFO "in_irq        = %ld\n", in_irq());
+	printk(KERN_INFO "in_hardirq    = %ld\n", in_hardirq());
+	printk(KERN_INFO "in_softirq    = %ld\n", in_softirq());
+	printk(KERN_INFO "in_interrupt  = %ld\n", in_interrupt());
+	printk(KERN_INFO "softirq_count = %ld\n", softirq_count());
+	printk(KERN_INFO "hardirq_count = %ld\n", hardirq_count());
+	printk(KERN_INFO "irq_count     = %ld\n", irq_count());
+	//return IRQ_NONE;
+	return IRQ_HANDLED;
 }
 
 
 static int __init rtoax_irq_init(void) {
     
 	printk(KERN_INFO "request irq %s!\n", name);
-    /*
-     *  注册中断
-     */
-    if (request_irq(IRQ_NUM, no_action, IRQF_NO_THREAD, name, NULL))
+    if (request_irq(irqnum, no_action, IRQF_SHARED, name, &mydev) != 0) {
 	    printk(KERN_ERR "%s: request_irq() failed\n", name);
+		return -1;
+	}
 	return 0;
 }
 
@@ -48,7 +58,7 @@ static void __exit rtoax_irq_cleanup(void) {
     /*
      *  释放中断
      */
-    free_irq(IRQ_NUM, NULL);
+    free_irq(irqnum, &mydev);
 }
 
 module_init(rtoax_irq_init);
