881d631e6109320f031e2c1cebccc9728c5ca086
rtoax <2386499836@qq.com>
2021-11-05 19:12:40 +0800

    打印 page 信息

---
+++ mm/page/modules/alloc_page.c
@@ -0,0 +1,71 @@
+/**
+ *	File	./alloc_page.c
+ *	Time	2021.11.05
+ *	Author	Rong Tao <rongtao@cestc.cn>
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/printk.h>
+#include <linux/gfp.h>
+#include <linux/mm.h>
+#include <linux/page_ref.h> //page_count()
+#include <linux/page-flags.h> //PG_xxxx
+
+
+/**
+ *	Print page information
+ */
+static void print_page(struct page* page)
+{
+	printk("flags     %lx\n", page->flags);
+	printk("_mapcount %d\n", page_mapcount(page));
+	printk("_refcount %d\n", page_count(page));
+	printk("PageAnon  %d\n", PageAnon(page));
+	printk("page2virt %p\n", page_to_virt(page));
+	printk("page2pfn  %lx\n", page_to_pfn(page));
+}
+static struct page* test__alloc(void)
+{
+	struct page *page = alloc_page(GFP_KERNEL|GFP_ATOMIC);
+	if(!page) {
+		printk("fail alloc_page.\n");
+	}
+	return page;
+}
+
+static void test__free(struct page *page)
+{
+	if(page) {
+		__free_pages(page, 0);
+	}
+}
+
+
+static int kernel_init(void)
+{
+#define npage 2
+	int ipage;
+	struct page *page[npage];
+
+    printk(KERN_INFO "========== my init =========\n");
+	for(ipage = 0; ipage < npage; ipage++) {
+		page[ipage] = test__alloc();
+		print_page(page[ipage]);
+	}
+	for(ipage = 0; ipage < npage; ipage++) {
+		test__free(page[ipage]);
+	}
+    return 0;
+}
+
+static void kernel_exit(void)
+{
+	printk(KERN_INFO "my exit.\n");
+}
+
+module_init(kernel_init);
+module_exit(kernel_exit);
+MODULE_AUTHOR("Rong Tao");
+MODULE_LICENSE("GPL");
