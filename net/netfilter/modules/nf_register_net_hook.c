5e016037075403565d3517b443b621b993e2b9c8
Rtoax <2386499836@qq.com>
2021-11-07 01:09:09 -0700

    nf_register_net_hook demo

--- net/netfilter/modules/nf_register_net_hook.c
+++ net/netfilter/modules/nf_register_net_hook.c
@@ -8,16 +8,36 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+
+static struct nf_hook_ops netfilter_ops;
+
+unsigned int main_hook(void *priv,
+		       struct sk_buff *skb,
+		       const struct nf_hook_state *state)
+{
+    printk(KERN_INFO "PF_INET netfilter hook called.\n");
+    return NF_ACCEPT;
+}
 
 static int kernel_init(void)
 {
-    printk(KERN_INFO "mykthread init.\n");
+    printk(KERN_INFO "my init.\n");
+    
+    netfilter_ops.hook = main_hook;
+    netfilter_ops.pf = PF_INET;
+    netfilter_ops.hooknum = 0;
+    netfilter_ops.priority = NF_IP_PRI_FIRST;
+    
+    nf_register_net_hook(&init_net, &netfilter_ops);
+    
     return 0;
 }
 
 static void kernel_exit(void)
 {
-
+    printk(KERN_INFO "my exit.\n");
+    nf_unregister_net_hook(&init_net, &netfilter_ops);
 }
 
 module_init(kernel_init);
