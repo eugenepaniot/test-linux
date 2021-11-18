e3d44b9a01662476547da7a52a3e57b97e836c0d
rtoax <2386499836@qq.com>
2021-11-18 19:32:14 +0800

    计算哈希值 hash_64; hash_long

---
+++ algorithm/hash/modules/hash_ptr.c
@@ -0,0 +1,46 @@
+/**
+ *	File	hash_ptr.c
+ *	Time	2021.11.18
+ *	Author	Rong Tao <rongtao@cestc.cn>
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/hash.h>
+
+static char *docs[] = {
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	NULL
+};
+
+static int kernel_init(void)
+{
+	int i = 0;
+	char *s;
+	unsigned int bits = 3;
+
+    printk(KERN_INFO "my init.\n");
+	
+	while((s = docs[i++])) {
+		printk("%d\n", hash_ptr((void*)s, bits));
+	}
+    return 0;
+}
+
+static void kernel_exit(void)
+{
+    printk(KERN_INFO "my exit.\n");
+}
+
+module_init(kernel_init);
+module_exit(kernel_exit);
+MODULE_AUTHOR("Rong Tao");
+MODULE_LICENSE("GPL");
