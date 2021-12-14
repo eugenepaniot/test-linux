62ea44b82a5d37f1368f9eba283356d92d85c2fd
rtoax <2386499836@qq.com>
2021-12-14 19:07:12 +0800

    tail -n 20

--- scripts/dmesg-tail.sh
+++ scripts/dmesg-tail.sh
@@ -1,9 +1,11 @@
 #!/bin/bash 
 
+set -euo pipefail
+
 function dmesg_tail() {
-	dmesg | tail
+	dmesg | tail -n 20
 }
 function dmesg_kernel_tail() {
-	dmesg -k | tail
+	dmesg -k | tail -n 20
 }
 dmesg_tail
