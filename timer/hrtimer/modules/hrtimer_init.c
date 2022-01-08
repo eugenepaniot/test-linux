9c36fab94f702e536e814d306c954d262d33ab32
rongtao <2386499836@qq.com>
2022-01-08 15:01:44 +0800

    hrtimer 代码优化

--- timer/hrtimer/modules/hrtimer_init.c
+++ timer/hrtimer/modules/hrtimer_init.c
@@ -13,47 +13,50 @@
 #include <linux/ktime.h>
 
 static struct hrtimer timer;
-s64 i64TimeInNsec = 500 * NSEC_PER_MSEC;
-enum hrtimer_restart enHRTimer=HRTIMER_NORESTART;
+s64 time_in_nsec = 500 * NSEC_PER_MSEC;
+enum hrtimer_restart restart=HRTIMER_NORESTART;
 
 /**
  *	
  */
 static enum hrtimer_restart timer_callback(struct hrtimer *timer)
 {
-    printk(KERN_ERR "Callback\n");
-    //I know something needs to go here to reset the timer
+	printk(KERN_INFO "Callback\n");
+	/**
+	 * Forward the timer expiry so it will expire in the future.
+	 * Returns the number of overruns.
+	 */
 	hrtimer_forward(timer, hrtimer_cb_get_time(timer),
-				ktime_set(0, i64TimeInNsec));
-    return enHRTimer;
+				ktime_set(0, time_in_nsec));
+	return restart;
 }
 
 static int init_timer(void)
 {   
-    ktime_t ktime;
+	ktime_t ktime;
 
-	enHRTimer = HRTIMER_RESTART;
-    printk(KERN_ERR "Timer being set up\n");
+	restart = HRTIMER_RESTART;
+	printk(KERN_ERR "Timer being set up\n");
 
-    ktime = ktime_set(0, i64TimeInNsec);
-    hrtimer_init(&timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	ktime = ktime_set(0, time_in_nsec);
+	hrtimer_init(&timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	hrtimer_set_expires(&timer, ktime);
-    timer.function = &timer_callback;
-    printk(KERN_ERR "Timer starting to fire\n");
-    printk(KERN_ERR "in %lldms %ld\n", i64TimeInNsec/NSEC_PER_MSEC,  jiffies);
+	timer.function = &timer_callback;
+	printk(KERN_ERR "Timer starting to fire\n");
+	printk(KERN_ERR "in %lldms %ld\n", time_in_nsec/NSEC_PER_MSEC,  jiffies);
 
-    hrtimer_start(&timer, ktime, HRTIMER_MODE_REL);
-    return 0;
+	hrtimer_start(&timer, ktime, HRTIMER_MODE_REL);
+	return 0;
 }
 
 static void clean_load_balancing_timer(void)
 {
-    int cancelled = hrtimer_cancel(&timer);
-	enHRTimer = HRTIMER_NORESTART;
-    if (cancelled)
-        printk(KERN_ERR "Timer still running\n");
-    else
-        printk(KERN_ERR "Timer cancelled\n");
+	int cancelled = hrtimer_cancel(&timer);
+	restart = HRTIMER_NORESTART;
+	if (cancelled)
+		printk(KERN_ERR "Timer still running\n");
+	else
+		printk(KERN_ERR "Timer cancelled\n");
 }
 
 module_init(init_timer);
