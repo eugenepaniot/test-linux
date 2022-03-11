600c958240d004462194e8dbaad72328fcddd785
Rong Tao <2386499836@qq.com>
2022-03-11 09:29:02 +0800

    简单对比 clock_gettime() 和 rdtsc

--- time/syscall/clock_gettime.c
+++ time/syscall/clock_gettime.c
@@ -1,6 +1,17 @@
+/**
+ * File: clock_gettime.c
+ * Discription: Compare the latency of vDSO, syscall, etc.
+ *
+ * Author:	Ge Changzhong (gechangzhong@cestc.cn)
+ *			Rong Tao (rongtao@cestc.cn)
+ * Time: 2022-03-11
+ *
+ * syscall, vDSO, rdtsc compare.
+ */
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <time.h>
 #include <assert.h>
 #include <string.h>
@@ -12,13 +23,39 @@
 typedef enum {
 	TEST_SYSCALL,
 	TEST_VSYSCALL,
-	TEST_VDSO
+	TEST_VDSO,
+#if defined(__x86_64__)
+	TEST_RDTSC,
+#endif
 } test_enum;
 
+#if defined(__x86_64__)
+static inline uint64_t __rdtsc(void)
+{
+   unsigned int a, d;
+   asm volatile ("rdtsc" : "=a" (a), "=d"(d));
+   return ((unsigned long) a) | (((unsigned long) d) << 32);
+}
+
+int rdtsc_clock_gettime(clockid_t clockid, struct timespec *tp)
+{
+	uint64_t tsc = __rdtsc();
+
+	/**
+	 * TODO: 注意，这里需要进一步计算才能获取 timespec
+	 */
+
+	return tsc;
+}
+#endif
+
+/* 使用系统调用 */
 int sys_clock_gettime(clockid_t clockid, struct timespec *tp)
 {
 	return syscall(__NR_clock_gettime, clockid, tp);
 }
+
+/* 系统默认使用 vDSO, 无需单独实现 clock_gettime() 接口 */
 
 void func(int cnt, test_enum te)
 {
@@ -34,6 +71,11 @@ void func(int cnt, test_enum te)
 	case TEST_SYSCALL:
 		test_clock_gettime = sys_clock_gettime;
 		break;
+#if defined(__x86_64__)
+	case TEST_RDTSC:
+		test_clock_gettime = rdtsc_clock_gettime;
+		break;
+#endif
 	case TEST_VSYSCALL:
 	default:
 		assert(0 && "Not support.");
@@ -54,7 +96,11 @@ void func(int cnt, test_enum te)
 
 void usage(int argc, char **argv)
 {
-	printf("Usage: %s [syscall|vdso]\n", argv[0]);
+	printf("Usage: %s [syscall|vdso"
+#if defined(__x86_64__)
+		"|rdtsc"
+#endif
+		"]\n", argv[0]);
 }
 
 int main(int argc, char *argv[])
@@ -71,8 +117,12 @@ int main(int argc, char *argv[])
 	if (!strcmp(argv[1], "vdso")) {
 		te = TEST_VDSO;
 	} else if (!strcmp(argv[1], "syscall")) {
-                te = TEST_SYSCALL;
-        } else {
+		te = TEST_SYSCALL;
+#if defined(__x86_64__)
+	} else if (!strcmp(argv[1], "rdtsc")) {
+		te = TEST_RDTSC;
+#endif
+	} else {
 		usage(argc, argv);
 		exit(1);
 	}
