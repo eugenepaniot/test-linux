05f0755c78b83217150424ce2378d635e1126698
root <root@192.168.29.168>
2022-03-12 15:56:27 +0800

    kswapd

--- mm/kswapd/scripts/mmap.c
+++ mm/kswapd/scripts/mmap.c
@@ -5,25 +5,54 @@
 #include <stdio.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+unsigned long Nsuccess = 0, Nfailed = 0;
+
+void sig_handler(int signum)
+{
+	switch (signum) {
+	case SIGALRM:
+		printf("Success: %ld\n", Nsuccess);
+		printf("Failed: %ld\n", Nfailed);
+		alarm(1);
+		break;
+	default:
+		break;
+	}
+}
 
 int main()
 {
 	int i;
 	char *p;
 	int pagesize = getpagesize();
+	struct rlimit limit;
 
+	signal(SIGALRM, sig_handler);
+
+	limit.rlim_cur = 1024*1024*1024;
+	limit.rlim_max = 1024*1024*1024;
+	prlimit(getpid(), RLIMIT_MEMLOCK, &limit, NULL);
 	mlockall(0);
-	
+	alarm(1);
+
 	for (;;) {
-		p = mmap(NULL, pagesize*10, PROT_READ|PROT_WRITE,
+#if 1
+		p = mmap(NULL, pagesize*1024, PROT_READ|PROT_WRITE,
 				MAP_ANON|MAP_SHARED, -1, 0);
 		if (p == MAP_FAILED) {
-			perror("mmap");
+			//perror("mmap");
+			Nfailed++;
 			continue;
 		}
-		for (i = 0; i < 10; i += pagesize) {
+		Nsuccess++;
+		for (i = 0; i < 1024; i += pagesize) {
 			p[i] = 'A';
 		}
+#endif
 	}
 	return 0;
 }
