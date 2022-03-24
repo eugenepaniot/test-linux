2e0865d220425188e4f7bd2c94d52e9e45336f54
Rong Tao <2386499836@qq.com>
2022-03-24 10:51:21 +0800

    task_active_pid_ns

---
+++ namespace/pid/modules/task_active_pid_ns.c
@@ -0,0 +1,66 @@
+/**
+ *	find pid namespace struct by PID
+ *	
+ *	Author	Rong Tao <rongtao@cestc.cn>
+ *	Time	2022-03-24
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/init.h>
+#include <linux/pid.h>
+#include <linux/pid_namespace.h>
+
+static int test_task_pid = 1;
+module_param(test_task_pid, int, 0660);
+
+static int __init find_pidtest_init(void)
+{
+	int PID = test_task_pid;
+	bool has_task;
+	struct pid *pid;
+	struct task_struct *task;
+	struct pid_namespace *pid_ns;
+
+	printk("find_pidtest_init.\n");
+
+	pid = find_get_pid(PID);
+	if(!pid) {
+		printk("Not exist PID %d\n", PID);
+		return -1;
+	}
+
+	has_task = pid_has_task(pid, PIDTYPE_PID);
+	if(has_task) {
+		printk("PID %d has task.\n", PID);
+	}
+
+	task = pid_task(pid, PIDTYPE_PID);
+	if(task) {
+		printk("PID %d task name %s\n", PID, task->comm);
+	}
+	
+	task = get_pid_task(pid, PIDTYPE_PID);
+	if(task) {
+		printk("PID %d task name %s\n", PID, task->comm);
+	}
+
+	pid_ns = task_active_pid_ns(task);
+	if (pid_ns) {
+		printk("PID %d task name %s, has ns pid_allocated = %u\n",
+		 PID, task->comm, pid_ns->pid_allocated);
+	}
+	return 0;
+}
+
+static void __exit find_pidtest_exit(void)
+{
+	printk("find_pidtest_exit.\n");
+}
+
+module_init(find_pidtest_init);
+module_exit(find_pidtest_exit);
+
+MODULE_AUTHOR("Rong Tao");
+MODULE_LICENSE("GPL");
